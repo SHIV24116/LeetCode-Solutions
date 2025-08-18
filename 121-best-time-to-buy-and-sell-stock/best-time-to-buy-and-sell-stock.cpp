5d77905ed67e0096991743e50f241479244ff0ad
Shivendra Singh <singhshivendra1945@gmail.com>
2025-08-18 16:03:12 +0530

    Time: 4 ms (18.00%) | Memory: 97.3 MB (60.58%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int n=prices.size();
         int min_price=prices[0];
         int profit=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             profit=max(profit,prices[i]-min_price);
             min_price=min(min_price,prices[i]);
         }
