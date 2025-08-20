66feb5ed0ce6d112d4ec2922512c761e5ea7107c
Shivendra Singh <singhshivendra1945@gmail.com>
2025-08-20 16:54:05 +0530

    Time: 7 ms (43.07%) | Memory: 26.2 MB (43.59%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,9 +4,7 @@ public:
         vector<int>v;
         unordered_map<int,int>m;
         int n=nums.size();
-        for(int i:nums){
-           m[i]++;
-        }
+        for(int i:nums) m[i]++;
         for(auto& [k,val]:m){
             if(val>n/3) v.push_back(k); 
         }
