4918ee28c1ebba53a033238c86157758a2bc3e91
Shivendra Singh <singhshivendra1945@gmail.com>
2025-10-10 16:07:50 +0530

    Time: 138 ms (66.90%) | Memory: 151.9 MB (24.30%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -4,15 +4,11 @@ public:
         int sum=INT_MIN;
         int n=energy.size();
         vector<int>v(n);
-        for(int i=energy.size()-1;i>=0;i--){
-            if(i+k<n){
-                v[i]=energy[i]+v[i+k];
-            }
+        for(int i=n-1;i>=0;i--){
+            if(i+k<n)  v[i]=energy[i]+v[i+k];
             else v[i]=energy[i];
             sum=max(sum,v[i]);
-            
         }
         return sum;
-        
     }
 };
