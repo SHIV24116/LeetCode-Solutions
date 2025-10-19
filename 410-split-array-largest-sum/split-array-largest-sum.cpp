360872bad2c83f409061260a3d062c2bf281c633
Shivendra Singh <singhshivendra1945@gmail.com>
2025-10-19 18:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (26.02%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -4,9 +4,9 @@ public:
         int l=0;
         int h=0;
         int ans=0;
-        for(int i=0;i<nums.size();i++){
-            h+=nums[i];
-            l=max(l,nums[i]);
+        for(int i:nums){
+            h+=i;
+            l=max(l,i);
         }while(l<=h){
             int m=(l+h)/2;
             if(cansplit(nums,m,k)){
@@ -19,12 +19,12 @@ public:
     bool cansplit(vector<int>& nums,int m,int k){
         int splits=1;
         int x=0;
-        for(int i=0;i<nums.size();i++){
-            if(x+nums[i]>m){
+        for(int i:nums){
+            if(x+i>m){
                 splits++;
                 x=0;
             }
-            x+=nums[i];
+            x+=i;
         }
         return splits<=k;
     }
