738cd4fbbd4eead472bf60585a772c9c7c1f9714
Shivendra Singh <singhshivendra1945@gmail.com>
2025-10-19 17:59:12 +0530

    Time: 3 ms (9.22%) | Memory: 10.4 MB (89.12%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int l=0;
+        int h=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            h+=nums[i];
+            l=max(l,nums[i]);
+        }while(l<=h){
+            int m=(l+h)/2;
+            if(cansplit(nums,m,k)){
+                ans=m;
+                h=m-1;
+            }else l=m+1;
+        }
+        return ans;    
+    }
+    bool cansplit(vector<int>& nums,int m,int k){
+        int splits=1;
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+            if(x+nums[i]>m){
+                splits++;
+                x=0;
+            }
+            x+=nums[i];
+        }
+        return splits<=k;
+    }
+        
+};
