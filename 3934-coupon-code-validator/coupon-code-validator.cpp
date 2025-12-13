ff8e97cbac3cb9b75227019d862d56555f867f6d
Shivendra Singh <singhshivendra1945@gmail.com>
2025-12-13 19:40:09 +0530

    Time: 7 ms (52.79%) | Memory: 68.8 MB (72.96%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string>ans;
+        vector<string>elec;
+        vector<string>groc;
+        vector<string>pharma;
+        vector<string>rest;
+        for(int i=0;i<code.size();i++){
+            bool x=true;
+            if(!isActive[i]) x=false;
+            if(businessLine[i]!="electronics" && businessLine[i]!="pharmacy" && businessLine[i]!="grocery" && businessLine[i]!="restaurant") x=false;
+            if(code[i]=="") x=false;
+            for(int j=0;j<code[i].size();j++){
+                bool v=true;
+                if((!isalnum(code[i][j]) && code[i][j]!='_')) v=false;
+                if(!v) x=false;
+            } 
+            if(x){
+                if(businessLine[i]=="electronics") elec.push_back(code[i]);
+                else if(businessLine[i]=="grocery") groc.push_back(code[i]);
+                else if(businessLine[i]=="pharmacy") pharma.push_back(code[i]);
+                else if(businessLine[i]=="restaurant") rest.push_back(code[i]);
+            }
+        }
+        sort(elec.begin(),elec.end());
+        sort(groc.begin(),groc.end());
+        sort(pharma.begin(),pharma.end());
+        sort(rest.begin(),rest.end());
+        for(int j=0;j<elec.size();j++){
+                ans.push_back(elec[j]);
+        } 
+        for(int j=0;j<groc.size();j++){
+                ans.push_back(groc[j]);
+        } 
+        for(int j=0;j<pharma.size();j++){
+                ans.push_back(pharma[j]);
+        } 
+        for(int j=0;j<rest.size();j++){
+                ans.push_back(rest[j]);
+        } 
+        return ans;
+    }
+};
