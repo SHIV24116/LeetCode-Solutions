2f6d0aef1ca27a88bb0d4a6eb1b4e880cb634006
Shivendra Singh <singhshivendra1945@gmail.com>
2026-01-07 00:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.67%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size()) return false;
-        int n=s.size();
-        while(n>0){
-            char x=s[0];
-            s.erase(0,1);
-            s=s+x;
-            n--;
-            if(s==goal) return true;
-        }return false;
+        if (s.size() != goal.size()) return false;
+        return (s + s).find(goal) != string::npos;  //string::npos denotes means the seach failed
     }
 };
