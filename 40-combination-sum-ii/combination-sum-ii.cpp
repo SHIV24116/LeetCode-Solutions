1c70c1200e7da6cbff02ef9e246eba3d3347e726
Shivendra Singh <singhshivendra1945@gmail.com>
2026-01-15 13:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (98.02%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void helper(int i,vector<int>&candidates,int target,vector<vector<int>>&ans,vector<int>&curr){
+        if(target==0){
+            ans.push_back(curr);
+            return;
+        }
+        for (int j = i; j < candidates.size(); j++) {
+            // Skip duplicates
+            if (j > i && candidates[j] == candidates[j - 1])
+                continue;
+
+            if (candidates[j] > target)
+                break;
+
+            curr.push_back(candidates[j]);
+            helper(j + 1, candidates, target - candidates[j], ans, curr);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); // REQUIRED
+        vector<vector<int>>ans;
+        vector<int>curr;
+        helper(0,candidates,target,ans,curr);
+        return ans;
+    }
+};
