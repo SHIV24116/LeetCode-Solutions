6f4a6448df7a820016679a4da73434d1615cab33
Shivendra Singh <singhshivendra1945@gmail.com>
2026-01-26 16:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (19.07%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string s="";
+        string s = "";
         int x = abs((int)a.size() - (int)b.size());
-        if(a.size()>b.size()){
-            while(x>0){
-                b='0'+b;
-                x--;
+        if (a.size() > b.size()) {
+            while (x--) b = '0' + b;
+        } else {
+            while (x--) a = '0' + a;
+        }
+        int c = 0;
+        s.reserve(a.size() + 1);
+        for (int i = a.size() - 1; i >= 0; --i) {
+            if (a[i] == '1' && b[i] == '1') {
+                if (c == 0) s.push_back('0');
+                else s.push_back('1');
+                c = 1;
+            }
+            else if ((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1')) {
+                if (c == 0) {
+                    s.push_back('1');
+                } else {
+                    s.push_back('0');
+                    c = 1;
+                }
+            }
+            else { 
+                if (c == 0) {
+                    s.push_back('0');
+                } else {
+                    s.push_back('1');
+                    c = 0;
+                }
             }
         }
-        else{
-            while(x>0){
-                a='0'+a;
-                x--;
-            }
-        }
-        int c=0;
-        for(int i=a.size()-1;i>=0;--i){
-           if(a[i]=='1' && b[i]=='1'){
-            if(c==0) s='0'+s;
-            else s='1'+s;
-            c=1;
-           }
-           if((a[i]=='1' && b[i]=='0') || (a[i]=='0' && b[i]=='1')){
-            if(c==0) s='1'+s;
-            else s='0'+s;
-           }
-           if(a[i]=='0' && b[i]=='0'){
-            if(c==0) s='0'+s;
-            else{
-                s='1'+s;
-                c=0;
-            }
-           }
-        }
-        if(c==1) s='1'+s;
+        if (c == 1) s.push_back('1');
+        reverse(s.begin(), s.end());
         return s;
     }
 };
