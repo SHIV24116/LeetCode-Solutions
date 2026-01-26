15a9ada81dfd769f6e5e2308bcc88b62b4ea1ca3
Shivendra Singh <singhshivendra1945@gmail.com>
2026-01-26 18:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (45.75%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+         int x = n;   // treat as 32-bit unsigned
+         int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+           res = (res << 1) | (x & 1);
+           x >>= 1;
+        }
+        return (int)res;
+    }
+};
