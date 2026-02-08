be695891f4b8d73e0c6b8f5375b8a87eec44ee15
Shivendra Singh <singhshivendra1945@gmail.com>
2026-02-08 14:24:39 +0530

    Time: 3 ms (88.28%) | Memory: 11.1 MB (32.42%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -6,9 +6,9 @@ public:
         int ans=0;
         int l=0;
         for(int r=0;r<n;r++){
-            freq[s[r]-'a']++;
+            freq[s[r]-'a']++;     
             while(freq[0]>0 && freq[1]>0 && freq[2]>0){
-                ans+= n-r;
+                ans+= n-r; /// sice r is 0 indexed so no need to add 1
                 freq[s[l]-'a']--;
                 l++;
             }
