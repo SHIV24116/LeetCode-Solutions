640b249ec735a2be90a2f8af85a1f3476dfdcf7d
Shivendra Singh <singhshivendra1945@gmail.com>
2026-02-14 18:51:33 +0530

    Time: 8 ms (25.25%) | Memory: 24.5 MB (19.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,25 +3,15 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>>v;
         sort(intervals.begin(),intervals.end());
-        vector<int>temp;
-        temp.push_back(intervals[0][0]);
-        temp.push_back(intervals[0][1]);
-        v.push_back(temp);
-        int k=0;
-        for(int i=1;i<=intervals.size();i++){
-            if(i<intervals.size() && intervals[i][0]<=temp[1] && intervals[i][1]>=temp[0]){
-                v[k][1]=max(temp[1],intervals[i][1]);
-                v[k][0]=min(temp[0],intervals[i][0]);
-                temp[1]=v[k][1];
-                temp[0]=v[k][0];
-            }
+        v.push_back(intervals[0]);
+        if(intervals.size()==1) return v;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]>v.back()[1]) v.push_back(intervals[i]);
             else{
-                if(i<intervals.size()){
-                    v.push_back(intervals[i]);
-                    k++;
-                    temp[0]=intervals[i][0];
-                    temp[1]=intervals[i][1];
-                }
+                int a=min(intervals[i][0],v.back()[0]);
+                int b=max(intervals[i][1],v.back()[1]);
+                v.pop_back();
+                v.push_back({a,b});
             }
         }
         return v;
