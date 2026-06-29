59317f7c48464d1be381bd40b3b40e2469fd7657
Shivendra Singh <singhshivendra1945@gmail.com>
2026-06-29 17:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (61.65%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int ct=0;
-        int n=word.size();
-        for(string s:patterns){
-            int x=s.size();
-            for(int i=0;i<n-x+1;i++){
-                if(s==word.substr(i,x)){
-                    ct++;
+        int ct = 0;
+        int n = word.size();
+        for (string s : patterns) {
+            int x = s.size();
+            bool v = false;
+            for (int st = 0; st <= n - x; st++) {      ////in this loop we are conceptually implementing word.find(s) which returns the first occurence of s in word and if not present returns  "std::string::npos"
+                int j = 0;
+                while (j < x && word[st + j] == s[j])
+                    j++;
+                if (j == x) {
+                    v = true;
                     break;
                 }
             }
+            if (v) ct++;
         }
         return ct;
-        
     }
 };
