c63f2bf76f31357c1cbb01f1a8981e52ca74560a
Shivendra Singh <singhshivendra1945@gmail.com>
2026-06-30 15:47:37 +0530

    Time: 7 ms (45.33%) | Memory: 20 MB (32.79%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,12 +3,12 @@ public:
     bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
         vis[node] = 1;
         for(auto it : adj[node]) {
-            if(vis[it] == 1) return true;
+            if(vis[it] == 1) return true;   //vis[it]=0....unvisited,1.....visiting,2.....visited
             if(vis[it] == 0){
-                if(dfs(it, adj, vis)) return true;
+                if(dfs(it, adj, vis)) return true;     //for node to become visited(completable course) all its prerequisites must be either 0(not explored yet) or 2(already completed)
             }
         }
-        vis[node] = 2;
+        vis[node] = 2;   //once all prerequisites of the node are explored....it is marked visited.
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -19,7 +19,7 @@ public:
         vector<int> vis(numCourses, 0);
         for(int i = 0; i < numCourses; i++) {
             if(vis[i] == 0) {
-                if(dfs(i, adj, vis)) return false;
+                if(dfs(i, adj, vis)) return false;    //dfs true means there is a cycle in the graph=>this course can not be completed
             }
         }
         return true;
