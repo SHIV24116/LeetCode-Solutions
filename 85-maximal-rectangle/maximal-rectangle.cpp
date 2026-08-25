84a35254bc2bcc73387087cd8dfe85cedc877d21
Shivendra Singh <singhshivendra1945@gmail.com>
2026-08-25 17:48:43 +0530

    Time: 4 ms (82.25%) | Memory: 19.1 MB (80.14%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -4,18 +4,14 @@ public:
         stack<int> st;
         int maxArea = 0;
         int n = heights.size();
-
         for (int i = 0; i <= n; i++) {
             int h = (i == n ? 0 : heights[i]);
-
             while (!st.empty() && h < heights[st.top()]) {
                 int height = heights[st.top()];
                 st.pop();
-
                 int right = i;
                 int left = st.empty() ? -1 : st.top();
                 int width = right - left - 1;
-
                 maxArea = max(maxArea, height * width);
             }
             st.push(i);
@@ -25,21 +21,17 @@ public:
 
     int maximalRectangle(vector<vector<char>>& matrix) {
         if (matrix.empty()) return 0;
-
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> height(m, 0);
+        vector<int> height(m, 0); //heights for each column from a particular row
 
         int ans = 0;
-
+        //we can convert the matrix into a histogram
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == '1')
-                    height[j] += 1;
-                else
-                    height[j] = 0;
+                if (matrix[i][j] == '1') height[j] += 1;
+                else height[j] = 0;
             }
-
             ans = max(ans, largestRectangleArea(height));
         }
         return ans;
