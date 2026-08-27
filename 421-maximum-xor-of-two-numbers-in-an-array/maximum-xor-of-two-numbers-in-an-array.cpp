e4268924db68d1834e3576244450c8cd37d68f59
Shivendra Singh <singhshivendra1945@gmail.com>
2026-08-27 18:15:33 +0530

    Time: 593 ms (73.98%) | Memory: 184.1 MB (77.26%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
-    int findMaximumXOR(vector<int>& nums) {
+    class Node {
+    public:
+        Node* children[2];
+        Node() {
+            for(int i = 0; i < 2; i++) {
+                children[i] = nullptr;
+            }
+        }
+    };
+    
+    Node* root=new Node();
+    // Insert a number into the Trie
+    void insert(int num) {
+        Node* curr = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(curr->children[bit] == nullptr) {
+                curr->children[bit] = new Node();
+            }
+            curr = curr->children[bit];
+        }
+    }
+    // Find maximum XOR possible with num
+    int findMaxXOR(int num) {
+        Node* curr = root;
         int ans = 0;
-        unordered_set<int> prefixes;
-
-        for (int bit = 31; bit >= 0; bit--) {
-            prefixes.clear();
-            for (int num : nums) {
-                prefixes.insert(num >> bit);
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            // For maximum XOR, we want the opposite bit
+            int opposite = 1 - bit;
+            if(curr->children[opposite] != nullptr) {
+                // This bit of XOR becomes 1
+                ans |= (1 << i);
+                curr = curr->children[opposite];
             }
-
-            int want = (ans << 1) | 1;
-
-            for (int p : prefixes) {
-                if (prefixes.count(p ^ want)) {
-                    ans = want;
-                    break;
-                }
+            else {
+                // Opposite bit doesn't exist
+                curr = curr->children[bit];
             }
-
-            if (ans != want) {
-                ans = ans << 1;
-            }
+        }
+        return ans;
+    }
+    int findMaximumXOR(vector<int>& nums) {
+        // Put all numbers into the Trie
+        for(int num : nums) {
+            insert(num);
+        }
+        int ans = 0;
+        // Find the best XOR for every number
+        for(int num : nums) {
+            ans = max(ans, findMaxXOR(num));
         }
         return ans;
     }
